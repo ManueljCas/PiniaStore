ad78c274f0aaba2d2247bfe7670193fff31e08f9
ManuelCastro <jesus.castro.pech@gmail.com>
2024-04-18 04:11:49 -0500

    Cambiar puerto

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    port: 1709
   },
   plugins: [
     vue(),
